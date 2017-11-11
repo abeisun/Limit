9abcb63c2b1e4d820bceb66ac277414d00ae9a90
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2017-11-11 15:42:57 -0500

    somewhat working drinks

--- app/src/main/java/com/limitwhack/limit/MainActivity.java
+++ app/src/main/java/com/limitwhack/limit/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
