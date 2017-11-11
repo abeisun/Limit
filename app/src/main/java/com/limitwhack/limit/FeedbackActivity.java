b435b37ff56eb7d4a7db9dc849ed7e3ad459e4b4
Angela <angelabsun@yahoo.com>
2017-11-11 18:23:30 -0500

    text edits

--- app/src/main/java/com/limitwhack/limit/FeedbackActivity.java
+++ app/src/main/java/com/limitwhack/limit/FeedbackActivity.java
@@ -2,11 +2,19 @@ package com.limitwhack.limit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import javax.xml.bind.annotation.*;
+
+import com.github.clans.fab.FloatingActionButton;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    FloatingActionButton happy;
+    FloatingActionButton meh;
+    FloatingActionButton sad;
+    FloatingActionButton ded;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
         setTitle("Feedback");
