5514bff20bbf9bb2c14c258b471f6f98261d4666
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2017-11-11 19:00:48 -0500

    added user class

---
+++ app/src/main/java/com/limitwhack/limit/User.java
@@ -0,0 +1,53 @@
+package com.limitwhack.limit;
+
+import io.realm.RealmObject;
+
+public class User extends RealmObject {
+
+    private Integer age;
+    private Integer weight;
+    private Integer height;
+    private String gender;
+    private String emergencyNumber;
+
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmergencyNumber() {
+        return emergencyNumber;
+    }
+
+    public void setEmergencyNumber(String emergencyNumber) {
+        this.emergencyNumber = emergencyNumber;
+    }
+}
