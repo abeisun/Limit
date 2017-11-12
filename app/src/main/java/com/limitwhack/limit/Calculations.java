79bf6afdbfdabef410fd9b2520d02e3ebc43a2c3
sdirie <32782238+sdirie@users.noreply.github.com>
2017-11-11 22:03:59 -0500

    wrote concetration calculation function

--- app/src/main/java/com/limitwhack/limit/Calculations.java
+++ app/src/main/java/com/limitwhack/limit/Calculations.java
@@ -2,6 +2,12 @@ package com.limitwhack.limit;
 
 public class Calculations {
     public static final double CONVERT_KILOGRAMS = 0.45359237;
+    public static final double BEER_CONCENTRATION = 0.05;
+    public static final double BEER_STANDARD_DRINK = 12;
+    public static final double WINE_CONCENTRATION = 0.12;
+    public static final double WINE_STANDARD_DRINK = 5;
+    public static final double HARD_LIQUOR_CONCENTRATION = 0.40;
+    public static final double HARD_LIQUOR_DRINK = 1.5;
 
     //Convert weight in pounds to weight in kilograms
     public double convertWeight(double weight) {
@@ -27,6 +33,12 @@ public class Calculations {
 
         return BACLevel;
     }
+
+    public double calculateConcentration(double standard_drinks) {
+        double concentration = standard_drinks * BEER_STANDARD_DRINK * BEER_CONCENTRATION;
+        return concentration;
+    }
+
 
 
 }
