3c523026fa21bca165464a12e89940eaedf38e6f
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2017-11-11 20:46:55 -0500

    added user to database

--- app/src/main/java/com/limitwhack/limit/StartActivity.java
+++ app/src/main/java/com/limitwhack/limit/StartActivity.java
@@ -3,15 +3,23 @@ package com.limitwhack.limit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 
+import io.realm.Realm;
+
 public class StartActivity extends AppCompatActivity {
     Button submit;
+    EditText weightEditText;
+    EditText emergencyContact;
+    Spinner genderSpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +28,43 @@ public class StartActivity extends AppCompatActivity {
         setTitle("Start");
 
         submit = findViewById(R.id.saveButton);
+        weightEditText = findViewById(R.id.weightInput);
+        emergencyContact = findViewById(R.id.contactEditText);
+        genderSpinner = findViewById(R.id.genderSpinner);
 
-       Spinner dropdown = (Spinner)findViewById(R.id.genderSpinner);
-       ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_array,
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_array,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        dropdown.setAdapter(adapter);
+        genderSpinner.setAdapter(adapter);
 
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                Realm realm = Realm.getDefaultInstance();
+
+                realm.executeTransactionAsync(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm bgRealm) {
+                        User user = new User();
+                        user.setGender(genderSpinner.getSelectedItem().toString());
+                        user.setEmergencyNumber(emergencyContact.getText().toString());
+                        user.setWeight(Integer.parseInt(weightEditText.getText().toString()));
+
+                         bgRealm.copyToRealm(user);
+                    }
+                }, new Realm.Transaction.OnSuccess() {
+                    @Override
+                    public void onSuccess() {
+                        Log.d("added user", "onSuccess: ");
+                    }
+                }, new Realm.Transaction.OnError() {
+                    @Override
+                    public void onError(Throwable error) {
+                        Log.d("fail to add user", error.toString());
+                    }
+                });
+
+
+
 
 
 
